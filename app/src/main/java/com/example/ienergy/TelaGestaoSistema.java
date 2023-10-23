72c7d7446af969cc2353103c8ce5e3dc16488652
PAULO ROBERTO BELEM DE SEIXAS <61366935268@professores.estacio.br>
2023-10-23 00:29:39 -0300

    Primeiro commit - Telas

---
+++ app/src/main/java/com/example/ienergy/TelaGestaoSistema.java
@@ -0,0 +1,14 @@
+package com.example.ienergy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaGestaoSistema extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_gestao_sistema);
+    }
+}
